98c9559d05658f8a43a7a82c85f527e77e41ab7f
igalna <igalna@gmail.com>
2014-10-06 16:33:41 +0100

    First commit of Palindrome program. Asks the user for some text. Checks to see if the text is Palindromic.

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        String userString;
+        String palindromeTest = "";
+        
+        System.out.print("Enter some text, the program will tell you if your text is" +
+                         " Palindromic.");
+        userString = keyboard.nextLine();
+        
+        for (int count = userString.length()-1; count > -1; count--) {
+            palindromeTest += userString.charAt(count);
+        }
+        
+        if (userString.equalsIgnoreCase(palindromeTest)) {
+            System.out.println(userString + " is a Palindrome of " + palindromeTest);
+        }
+        else {
+            System.out.println(userString + " is not a Palindrome of " + palindromeTest);
+        }
+    }
+}
